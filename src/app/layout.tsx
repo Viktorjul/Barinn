4e805c8330155409e5cc7dedb6a9e7edb6d5ce30
Viktor Júlíusson <viktorjul1406@gmail.com>
2025-12-25 16:56:29 +0000

    initial color and font setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,27 @@
 import type { Metadata } from "next";
+import { Climate_Crisis, Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
+
+const climateCrisis = Climate_Crisis({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-custom",
+  display: "swap",
+});
+
+const subtextFont = Playfair_Display({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-orange-squash",
+  display: "swap",
+});
+
+const paragraphFont = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--font-paragraph",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Barinn",
@@ -13,8 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${climateCrisis.className} ${climateCrisis.variable} ${subtextFont.variable} ${paragraphFont.variable}`}>{children}</body>
     </html>
   );
 }
-
